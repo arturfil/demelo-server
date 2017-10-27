d798dfc1b51cfd71da1af9ac57e26d15ca8607d3
Arturo Filio Villa <arturfil@hotmail.com>
2017-10-27 10:59:19 -0400

    first commit

---
+++ routes/auth-api-router.js
@@ -0,0 +1,9 @@
+const express  = require('express');
+const bcrypt   = require('bcyprt');
+const passport = require('passport');
+
+const UserModel = require('../models/user-model.js');
+
+const router = express.Router();
+
+module.exports = router;
